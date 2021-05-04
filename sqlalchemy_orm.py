ac2bd25de7ea20135326cf69276162b22fa49792
d-g-code <>
2021-05-04 16:12:14 +0200

    Adde session connect, create classroom and student record, modify and delete record

--- sqlalchemy_orm.py
+++ sqlalchemy_orm.py
@@ -35,3 +35,44 @@ class Student(SQLAlchemyModel):
 SQLAlchemyModel.metadata.create_all(database)
 
 
+# create session thant stores objects and allows talk to database
+DatabaseSession = sessionmaker(bind=database)
+session = DatabaseSession()
+
+# add two classes, if table is empty
+if not session.query(Classroom).count():
+    session.add(Classroom(name='1A', profile='programing'))
+    session.add(Classroom(name='1B', profile='web'))
+
+
+# create instance Classroom represent "1A"
+instance_classroom_1a = session.query(Classroom).filter_by(name='1A').one()
+
+# added data many students
+session.add_all([
+    Student(firstname='Jan', surname='Kowalski', classroom_id=instance_classroom_1a.id),
+    Student(firstname='Janusz', surname='Nowak', classroom_id=instance_classroom_1a.id),
+    Student(firstname='Janek', surname='Nowacki', classroom_id=instance_classroom_1a.id)
+])
+
+
+def read_data():
+    for student in session.query(Student).join(Classroom).all():
+        print(student.id,
+              student.firstname,
+              student.surname,
+              student.classroom_id)
+
+
+# change classroom student with id 2
+instance_student = session.query(Student).filter(Student.id == 2).one()
+instance_student.classroom_id = session.query(Classroom.id).filter(Classroom.name == '1B').scalar()
+
+read_data()
+
+# delete student with id 3
+session.delete(session.query(Student).get(3))
+
+# save changes and close session
+session.commi()
+session.close()
